33e80ee74ad94bf19522f4d0e7f9793432198c1d
Brandon Schafer <bpsr5b@mst.edu>
2013-03-31 17:30:42 -0500

    Initial Commit

---
+++ code/macrails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Macrails::Application.config.secret_token = 'a661b20d3ca3f7865a8266d38a4af4d6e19460f7e880f5d93ab8db37d8f1e723fde39846ce111110c5d5aa4d518d9a50bfe3dd3537259dd2f770aa933843e91f'
